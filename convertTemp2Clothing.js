c416b982d81b344ee40391423176eb1ab79a7a8b
Jagdeep Singh <59809683+jagdeepsgill888@users.noreply.github.com>
2025-01-25 03:58:50 +0000

    Question2 update

--- convertTemp2Clothing.js
+++ convertTemp2Clothing.js
@@ -1,7 +1,25 @@
 // 2) Take a variable with the temperature and another variable with the relative humidity and output a clothing suggestion.
+//let humid = 80;
+//let temp = 10;
+//let clothing = convertTemp2Clothing(temp, humid);
 
 function convertTemp2Clothing(temp, humid) {
-    return(`In a ${temp}c with ${humid}% humidity , bring a ${clothing}`);
+let clothing;
+    if(humid >70 && temp >3){
+        clothing = "Umbrella";
+    }else if (humid >70 && temp <3){
+        clothing = " Winter Jacket for snow";
+    }else if (humid <70 && temp <3){
+        clothing = "Warm Down jacket";
+    }else if (humid <70 && temp >3){
+        clothing = "Coat";
+    }
+    return clothing;
 }
+
+//console.log(`In a ${temp}c with ${humid}% humidity, bring a ${clothing}`);
+
+//return(`In a ${temp}c with ${humid}% humidity , bring a ${clothing}`);
+//}
 
 export {convertTemp2Clothing}
--- spec/converttemp_spec.js
+++ spec/converttemp_spec.js
@@ -1,27 +1,11 @@
 import { convertTemp2Clothing } from "../convertTemp2Clothing.js";
 
-describe("test convertTemp2Clothing ", function() {
-    it("tests 20c with 90% humidity", function() {
-      let temp = 20;
-      let humid = 90;
-      let clothing = convertTemp2Clothing(temp, humid);
-      expect(clothing).toBe("Jacket");
+describe("test convertTemp2Clothing", function() {
+    it("tests humid <70 and temp >3", function() {
+      let humid = 60;
+      let temp = 4;
+      let clothing = convertTemp2Clothing(temp, humid)
+      expect(clothing).toBe("Coat");
     });
-    it("tests 70m", function() {
-        let meters = 70;
-        let yards = convertMeters2Yards(meters);
-        expect(yards.toFixed(2)).toBe("76.58");
-      });
-
   });
   
-
-let meters = 150;
-let yards = convertMeters2Yards(meters);
-
-
-//let clothing = Jacket;
- if(temp >=3 && humid >=80 {
-    clothing = "jacket";
-} if else (temp <=2 && humid >=80 {
-    clothing = "jacket";
